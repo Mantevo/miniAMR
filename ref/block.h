6bec84c2cb69bfaa973f3cab43707d958c1d7470
ctvaugh <ctvaugh@sandia.gov>
2021-03-26 08:47:38 -0600

    Add files via upload
    Changes need for Clang compiler - when there is a global variable with the Clang compiler, it has to be declared only once and referenced as "extern" from other files.

--- ref/block.h
+++ ref/block.h
@@ -24,6 +24,12 @@
 //
 // ************************************************************************
 
+#ifdef MA_MAIN
+#define EXTERN
+#else
+#define EXTERN extern
+#endif
+
 //typedef int num_sz;
 typedef long long num_sz;
 
@@ -44,7 +50,7 @@ typedef struct {
    int cen[3];
    double ****array;
 } block;
-block *blocks;
+EXTERN block *blocks;
 
 typedef struct {
    num_sz number;
@@ -59,81 +65,81 @@ typedef struct {
    int child_node[8];
    int cen[3];
 } parent;
-parent *parents;
+EXTERN parent *parents;
 
 typedef struct {
    num_sz number;     // number of block
    int n;          // position in block array
 } sorted_block;
-sorted_block *sorted_list;
-int *sorted_index;
+EXTERN sorted_block *sorted_list;
+EXTERN int *sorted_index;
 
-int my_pe;
-int num_pes;
+EXTERN int my_pe;
+EXTERN int num_pes;
 
-int max_num_blocks;
-int num_refine;
-int uniform_refine;
-int x_block_size, y_block_size, z_block_size;
-int num_vars;
-int comm_vars;
-int init_block_x, init_block_y, init_block_z;
-int reorder;
-int npx, npy, npz;
-int inbalance;
-int refine_freq;
-int report_diffusion;
-int error_tol;
-int num_tsteps;
-int use_time;
-double end_time;
-int stages_per_ts;
-int checksum_freq;
-int stencil;
-int report_perf;
-int plot_freq;
-int num_objects;
-int lb_opt;
-int block_change;
-int code;
-int permute;
-int nonblocking;
-int refine_ghost;
-int change_dir;
-int group_blocks;
-int limit_move;
-int send_faces;
-int use_rcb;
+EXTERN int max_num_blocks;
+EXTERN int num_refine;
+EXTERN int uniform_refine;
+EXTERN int x_block_size, y_block_size, z_block_size;
+EXTERN int num_vars;
+EXTERN int comm_vars;
+EXTERN int init_block_x, init_block_y, init_block_z;
+EXTERN int reorder;
+EXTERN int npx, npy, npz;
+EXTERN int inbalance;
+EXTERN int refine_freq;
+EXTERN int report_diffusion;
+EXTERN int error_tol;
+EXTERN int num_tsteps;
+EXTERN int use_time;
+EXTERN double end_time;
+EXTERN int stages_per_ts;
+EXTERN int checksum_freq;
+EXTERN int stencil;
+EXTERN int report_perf;
+EXTERN int plot_freq;
+EXTERN int num_objects;
+EXTERN int lb_opt;
+EXTERN int block_change;
+EXTERN int code;
+EXTERN int permute;
+EXTERN int nonblocking;
+EXTERN int refine_ghost;
+EXTERN int change_dir;
+EXTERN int group_blocks;
+EXTERN int limit_move;
+EXTERN int send_faces;
+EXTERN int use_rcb;
 
-int first;
-int *dirs;
-int num_cells;
-int mat;
-int max_num_parents;
-int num_parents;
-int max_active_parent;
-int cur_max_level;
-num_sz *num_blocks;
-num_sz *local_num_blocks;
-num_sz *block_start;
-int num_active;
-int max_active_block;
-num_sz global_active;
-int x_block_half, y_block_half, z_block_half;
-double tol;
-double *grid_sum;
-num_sz *p8;
-int *p2;
-int mesh_size[3];
-int max_mesh_size;
-int *from, *to;
-int msg_len[3][4];
-int local_max_b;
-int global_max_b;
-double *a0, a1;
-double total_fp_divs;
-double total_fp_adds;
-double total_fp_muls;
+EXTERN int first;
+EXTERN int *dirs;
+EXTERN int num_cells;
+EXTERN int mat;
+EXTERN int max_num_parents;
+EXTERN int num_parents;
+EXTERN int max_active_parent;
+EXTERN int cur_max_level;
+EXTERN num_sz *num_blocks;
+EXTERN num_sz *local_num_blocks;
+EXTERN num_sz *block_start;
+EXTERN int num_active;
+EXTERN int max_active_block;
+EXTERN num_sz global_active;
+EXTERN int x_block_half, y_block_half, z_block_half;
+EXTERN double tol;
+EXTERN double *grid_sum;
+EXTERN num_sz *p8;
+EXTERN int *p2;
+EXTERN int mesh_size[3];
+EXTERN int max_mesh_size;
+EXTERN int *from, *to;
+EXTERN int msg_len[3][4];
+EXTERN int local_max_b;
+EXTERN int global_max_b;
+EXTERN double *a0, a1;
+EXTERN double total_fp_divs;
+EXTERN double total_fp_adds;
+EXTERN double total_fp_muls;
 
 typedef struct {
    int type;
@@ -146,11 +152,11 @@ typedef struct {
    double orig_size[3];
    double inc[3];
 } object;
-object *objects;
+EXTERN object *objects;
 
-int num_dots;
-int max_num_dots;
-int max_active_dot;
+EXTERN int num_dots;
+EXTERN int max_num_dots;
+EXTERN int max_active_dot;
 typedef struct {
    num_sz number;
    int n;
@@ -158,7 +164,7 @@ typedef struct {
    int new_proc;
    int cen[3];
 } dot;
-dot *dots;
+EXTERN dot *dots;
 typedef struct {
    num_sz number;
    num_sz num_prime;
@@ -166,4 +172,4 @@ typedef struct {
    int proc;
    int new_proc;
 } spot;
-spot *spots;
+EXTERN spot *spots;
